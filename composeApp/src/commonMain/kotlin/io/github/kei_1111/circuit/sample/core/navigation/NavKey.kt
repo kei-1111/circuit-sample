e7d70ee3365ec438f7a868232ec0cc55e9b2cbd6
kei-1111 <kei_02_1111@icloud.com>
2025-12-10 17:19:39 +0900

    chore: Nav3で使用するHomeの画面を定義

---
+++ composeApp/src/commonMain/kotlin/io/github/kei_1111/circuit/sample/core/navigation/NavKey.kt
@@ -0,0 +1,7 @@
+package io.github.kei_1111.circuit.sample.core.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Home : NavKey
