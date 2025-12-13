d6105b2c80bf5c121d59b5c7f9633b75e008d19d
kei-1111 <kei_02_1111@icloud.com>
2025-12-13 15:51:39 +0900

    chore: FavoriteTopAppBarを表示させるように変更

--- composeApp/src/commonMain/kotlin/io/github/kei_1111/circuit/sample/feature/main/favorite/Favorite.kt
+++ composeApp/src/commonMain/kotlin/io/github/kei_1111/circuit/sample/feature/main/favorite/Favorite.kt
@@ -1,15 +1,21 @@
 package io.github.kei_1111.circuit.sample.feature.main.favorite
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import com.slack.circuit.codegen.annotations.CircuitInject
 import io.github.kei_1111.circuit.sample.di.AppScope
+import io.github.kei_1111.circuit.sample.feature.main.favorite.component.FavoriteTopAppBar
 
 @CircuitInject(FavoriteScreen::class, AppScope::class)
 @Composable
@@ -17,15 +23,13 @@ fun Favorite(
     state: FavoriteState,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "お気に入り",
-            style = MaterialTheme.typography.headlineMedium,
-            color = MaterialTheme.colorScheme.onSurface
-        )
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            FavoriteTopAppBar()
+        }
+    ) { innerPadding ->
+
     }
 }
 
