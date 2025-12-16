9dfa4aea986c6bbf0d97f51854c322d4e952bc0c
kei-1111 <kei_02_1111@icloud.com>
2025-12-16 17:19:45 +0900

    chore: LibrariesContainerで表示するものをアプリに合わせるように変更

--- feature/oss/src/commonMain/kotlin/io/github/kei_1111/circuit/sample/feature/oss/Oss.kt
+++ feature/oss/src/commonMain/kotlin/io/github/kei_1111/circuit/sample/feature/oss/Oss.kt
@@ -1,14 +1,22 @@
 package io.github.kei_1111.circuit.sample.feature.oss
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import circuit_sample.feature.oss.generated.resources.Res
+import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
+import com.mikepenz.aboutlibraries.ui.compose.m3.libraryColors
 import com.mikepenz.aboutlibraries.ui.compose.produceLibraries
 import com.slack.circuit.codegen.annotations.CircuitInject
 import io.github.kei_1111.circuit.sample.core.common.AppScope
@@ -16,6 +24,7 @@ import io.github.kei_1111.circuit.sample.core.designsystem.theme.CircuitSampleTh
 import io.github.kei_1111.circuit.sample.core.navigation.OssScreen
 import io.github.kei_1111.circuit.sample.feature.oss.component.OssTopAppBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @CircuitInject(OssScreen::class, AppScope::class)
 @Composable
 fun Oss(
@@ -36,7 +45,28 @@ fun Oss(
             libraries = libraries,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .padding(innerPadding),
+            libraryModifier = Modifier.clip(RoundedCornerShape(8.dp)),
+            contentPadding = PaddingValues(16.dp),
+            colors = LibraryDefaults.libraryColors(
+                libraryBackgroundColor = MaterialTheme.colorScheme.surfaceContainer,
+                libraryContentColor = MaterialTheme.colorScheme.onSurface,
+            ),
+            padding = LibraryDefaults.libraryPadding(
+                contentPadding = PaddingValues(8.dp)
+            ),
+            dimensions = LibraryDefaults.libraryDimensions(
+                itemSpacing = 16.dp
+            ),
+            textStyles = LibraryDefaults.libraryTextStyles(
+                nameTextStyle = MaterialTheme.typography.titleMedium,
+                versionTextStyle = MaterialTheme.typography.labelMedium,
+                authorTextStyle = MaterialTheme.typography.labelSmall,
+                licensesTextStyle = MaterialTheme.typography.labelMedium,
+            ),
+            shapes = LibraryDefaults.libraryShapes(
+                chipShape = RoundedCornerShape(4.dp),
+            ),
         )
     }
 }
